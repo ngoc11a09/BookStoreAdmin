e513455be79947dd3cae0ce1f6aafa00ab30814a
Ngoc Khuu <khuuthibichngoc999@gmail.com>
2024-04-12 23:21:14 +0700

    fix signin validate

--- src/components/SignInForm.vue
+++ src/components/SignInForm.vue
@@ -2,9 +2,9 @@
     <a-card>
         <a-typography-title :style="{ textAlign: 'center' }">Sign In</a-typography-title>
         <a-flex justify="center" alignItems="center">
-            <a-form :model="formState" class="signin-form" name="signin" autocomplete="off" @submit="handleSubmit">
-                <a-form-item name="username"
-                    :rules="[{ required: true, message: 'Please input your username!' }, { min: 4, max: 25, message: 'Length should be 4 to 25', trigger: 'blur' },]">
+            <a-form :rules="rules" :model="formState" ref="formRef" class="signin-form" name="signin" autocomplete="off"
+                @submit="handleSubmit">
+                <a-form-item name="username">
                     <a-typography-text>Username:</a-typography-text>
                     <a-input v-model:value="formState.username">
                         <template #prefix>
@@ -13,8 +13,7 @@
                     </a-input>
                 </a-form-item>
 
-                <a-form-item name="password"
-                    :rules="[{ required: true, message: 'Please input your password!' }, { min: 8, max: 25, message: 'Length should be 8 to 25', trigger: 'blur' },]">
+                <a-form-item name="password">
                     <a-typography-text>Password:</a-typography-text>
                     <a-input-password v-model:value="formState.password">
                         <template #prefix>
@@ -40,26 +39,51 @@
 import type { ISignInData } from '@/interfaces/auth.interface';
 import { useAuthStore } from '@/stores/auth.store';
 import { UserOutlined, LockOutlined } from '@ant-design/icons-vue';
-import { reactive } from 'vue';
+import type { Rule } from 'ant-design-vue/es/form';
+import { reactive, ref, toRaw } from 'vue';
 
+const formRef = ref();
 const formState = reactive<ISignInData>({
-    username: "user",
-    password: "12345678"
+    username: "",
+    password: ""
 })
 
-const handleSubmit = async () => {
-    try {
-        const authStore = useAuthStore()
-        const res = await authStore.signIn(formState)
-        alert(res)
-    } catch (error: any) {
-        //console.log(error);
-        alert(error.message)
-        formState.username = ""
-        formState.password = ""
-    }
+const rules: Record<string, Rule[]> = {
+    username:
+        [
+            { required: true, message: 'Please input your username!' },
+            { min: 4, max: 25, message: 'Length should be 4 to 25' }
+        ],
+    password:
+        [
+            { required: true, message: 'Please input your password!' },
+            { min: 8, max: 25, message: 'Length should be 8 to 25' }
+        ]
 }
 
+const handleSubmit = async () => {
+    const res = await formRef.value
+        .validate()
+        .then(() => {
+            console.log('Validate successfully');
+        })
+        .catch((error: any) => {
+            return error.errorFields.length
+        });
+    if (res === 0) {
+        try {
+            const authStore = useAuthStore()
+            const res = await authStore.signIn(formState)
+            alert(res)
+        } catch (error: any) {
+            // console.log(error);
+            alert(error.message)
+            formState.password = ""
+        }
+    }
+
+
+}
 </script>
 
 <style>
