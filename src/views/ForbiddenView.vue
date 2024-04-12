2d31f68b566278d2f4c1ab91bc2db9ab0b5e8ea9
Ngoc Khuu <khuuthibichngoc999@gmail.com>
2024-04-13 01:35:54 +0700

    fix forbiddent page

--- src/views/ForbiddenView.vue
+++ src/views/ForbiddenView.vue
@@ -1,7 +1,7 @@
 <template>
     <a-result status="403" title="403" sub-title="Sorry, you are not authorized to access this page.">
         <template #extra>
-            <a-button @click="$router.push('/signin')" type="primary">Back Home</a-button>
+            <a-button @click="$router.push('/signin')" type="primary">Back to Signin</a-button>
         </template>
     </a-result>
 </template>
